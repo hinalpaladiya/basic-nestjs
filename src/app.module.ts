c6b8fef3c4c8152d397612c16ec9951930f18add
meet paladiya <meetpaladiya981@gmail.com>
2024-10-01 22:33:04 +0530

    database and added product resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+       type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
